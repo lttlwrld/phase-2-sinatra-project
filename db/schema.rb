cffd772cda6d849d93ef807224576a97be622cb0
andy <aytouch1993@icloud.com>
2022-08-19 13:20:31 -0500

    added name to Users

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 4) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "name"
     t.string "email"
     t.string "password_digest"
   end
