88b490f5b55a15c833b9162bf036690803359ff4
andy <aytouch1993@icloud.com>
2022-08-19 15:00:46 -0500

    save so i can work @ home

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,24 @@
 class ProjectsController < ApplicationController
 
-    get '/projects' do
+    get '/projects/new' do
+        if logged_in?
+            erb :"projects/new"
+        else
+            redirect '/login'
+        end
+    end
+
+    post '/projects/new' do
+        if logged_in?
+            if params[""] != ""
+                @project = Project.create(user_id: current_user.id)
+                redirect "/projects/#{@project.id}"
+            else
+                redirect '/projects/new'
+            end
+        else
+            redirect '/login'
+        end
     end
 
     get '/projects/:id' do
